e28f2fef9eb024597f77b43dd0d375018292f31c
Jose Garrido <141913305+garridojose@users.noreply.github.com>
2023-11-09 19:04:36 +0800

    student-api_4.5

--- student-api/src/main/java/com/it1311l/uap/studentapi/repository/StudentMybatisRepository.java
+++ student-api/src/main/java/com/it1311l/uap/studentapi/repository/StudentMybatisRepository.java
@@ -1,10 +1,20 @@
 package com.it1311l.uap.studentapi.repository;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.it1311l.uap.studentapi.model.StudentInfo;
+import com.it1311l.uap.studentapi.model.StudentRatings;
 
+@Mapper
 public interface StudentMybatisRepository {
-	@Insert("INSERT INTO attendees(firstName, middleName, lastName, course, age) VALUES (#{firstName}, #{middleName}, #{lastName}, #{course}, #{age})")
+	@Insert
+	("INSERT INTO student_info(studentName, avgGrade) VALUES (#{studentName}, #{avgGrade})" + "INSERT INTO student_ratings(name, grade) VALUES (#{name}, #{grade})")
 	public int insert(StudentInfo student_info);
+	public int insert(StudentRatings student_rating);
 }
